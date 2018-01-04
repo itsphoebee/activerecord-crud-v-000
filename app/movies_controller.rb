454f388917a62313c6b1e5fdb90893e064dddd8f
Phoebe Lau <phoebelau7@gmail.com>
2018-01-04 02:41:42 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -62,7 +62,7 @@ def can_be_found_updated_and_saved
   Movie.create(title: "Awesome Flick")
   Movie.find_by(title:"Awesome Flick")
   Movie.update(title: "Even Awesomer Flick")
-  .save
+  Movie.save
 end
 
 def can_update_using_update_method
